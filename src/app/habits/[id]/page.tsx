d11ea51faca5feddeeb389886653b0fbad9a8969
manabu-yamazaki <manabu.yamazaki.business@gmail.com>
2025-04-19 11:38:55 +0900

    エラー修正

--- src/app/habits/[id]/page.tsx
+++ src/app/habits/[id]/page.tsx
@@ -1,16 +1,10 @@
 import { HabitForm } from '@/components/HabitForm';
-import { Metadata } from 'next';
 
 type Props = {
   params: { id: string }
-  searchParams: { [key: string]: string | string[] | undefined }
 }
 
-export const metadata: Metadata = {
-  title: '習慣の編集',
-};
-
-export default async function HabitEditPage({
+export default function HabitEditPage({
   params,
 }: Props) {
   return <HabitForm habitId={params.id === 'new' ? undefined : params.id} />;
